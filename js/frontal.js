dc2aabe1e5f590f09b642999c64122510ee2e6e5
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2024-01-20 16:35:38 +0200

    add  frontal page to index.html

---
+++ js/frontal.js
@@ -0,0 +1,46 @@
+const textBehind = document.getElementById('text-behind');
+const textFront = document.getElementById('text-front');
+const textBehindBlur = document.getElementById('text-behind-blur');
+const canvasRect = document.getElementById('canvas');
+
+const parallaxScaling1 = 0.002;
+const parallaxScaling2 = 0.001;
+const parallaxScaling3 = 0.000001;
+
+let currentScroll = 0;
+let targetScroll = 0;
+let ease = 0.001;
+
+let theta1 = 0;
+
+function updateScale() {
+  let rect = canvasRect.getBoundingClientRect();
+  
+  let startScrollPosition = window.pageYOffset + rect.top; 
+  let endScrollPosition = window.pageYOffset + rect.bottom;
+
+  if (targetScroll + window.innerHeight < startScrollPosition || targetScroll > endScrollPosition) {
+     return;
+  }
+  
+  currentScroll += (targetScroll - currentScroll) * ease;
+  
+  let scaleValue1 = 1 + (currentScroll * parallaxScaling1);
+  let scaleValue2 = 1 + (currentScroll * parallaxScaling2);
+    
+  textBehind.style.transform = `scale(${scaleValue1})`;
+  textFront.style.transform = `scale(${scaleValue1})`;
+  textBehindBlur.style.transform = `scale(${scaleValue1})`;
+  canvasRect.style.transform = `scale(${scaleValue2})`;
+  
+  theta1 += currentScroll * parallaxScaling3;
+    
+  setTimeout(updateScale, 1500 / 60); 
+}
+
+window.addEventListener('scroll', () => {
+    targetScroll = window.pageYOffset;
+    updateScale();
+});
+
+updateScale();
