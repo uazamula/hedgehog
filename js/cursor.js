6bc7619df0794bab8419f40356b1049f94f79c25
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2024-02-02 19:12:35 +0200

    Format all files

--- js/cursor.js
+++ js/cursor.js
@@ -1,91 +1,61 @@
-
-var al = [];
+const al = [];
 function setup() {
-  createCanvas( windowWidth, windowHeight );
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background( '#11151c' );
-  
-  /*
-  * Creation of dotted background
-  */
-  stroke( '#bdbdbd' ) // Dot color
-  strokeWeight( 3 );  // Dot thickness
-  for( var i = 0; i < width; i = i + 80 ) {
-    for( var j = 0; j < height; j = j + 80 ) {
-      point(i, j);
-    }                         
-  }
-  
-  strokeWeight( 1 ); // Restore strokeWeight 
-  
-  /*
-  * Array to store reference to each Rays() object 
-  */
-  al.push( new Rays() );
-  
-  for( var i = 0; i < al.length; i++ ) {
-    var r = al[i];
-    r.applyForce( new p5.Vector( random( -0.5, 0.5 ), random( 0.01, 0.05 ) ) );
-    r.update();
-    r.render();
-    if( r.isDead() )
-      al.shift();
-  }
+    background("#11151c");
+
+    stroke("#bdbdbd");
+    strokeWeight(3);
+    for (var i = 0; i < width; i = i + 80) {
+        for (var j = 0; j < height; j = j + 80) {
+            point(i, j);
+        }
+    }
+
+    strokeWeight(1);
+
+    al.push(new Rays());
+
+    for (var i = 0; i < al.length; i++) {
+        var r = al[i];
+        r.applyForce(new p5.Vector(random(-0.5, 0.5), random(0.01, 0.05)));
+        r.update();
+        r.render();
+        if (r.isDead()) al.shift();
+    }
 }
 
 function windowResized() {
-  resizeCanvas( windowWidth, windowHeight );
+    resizeCanvas(windowWidth, windowHeight);
 }
 
 function Rays() {
-  this.counter = 0;
-  this.position = new p5.Vector( mouseX, mouseY );
-  this.velocity = new p5.Vector( 0, 0 );
-  this.acceleration = new p5.Vector( 0, 0 );
-  this.lifeSpan = 1;
+    this.counter = 0;
+    this.position = new p5.Vector(mouseX, mouseY);
+    this.velocity = new p5.Vector(0, 0);
+    this.acceleration = new p5.Vector(0, 0);
+    this.lifeSpan = 1;
 }
 
-/*
-* Takes p5.Vector object as the initial force 
-* This force provides the required acceleration
-*/
-Rays.prototype.applyForce = function( force ) {
-  acceleration = force;
-}
+Rays.prototype.applyForce = function (force) {
+    acceleration = force;
+};
 
-/*
-* Calculates and updates
-*
-* 1. Velocity
-* 2. Position
-*/
-Rays.prototype.update = function() {
-  this.velocity.add( this.acceleration );
-  this.position.add( this.velocity );
-  this.lifeSpan -= 0.04;
-}
+Rays.prototype.update = function () {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifeSpan -= 0.04;
+};
 
-/*
-* Displays line on the document
-*/
-Rays.prototype.render = function() {
-  var c = color( 'rgb(57, 102, 128, ' + this.lifeSpan + ')' );
-  stroke( c ); 
-  line( this.position.x, this.position.y, pmouseX, pmouseY );
-}
+Rays.prototype.render = function () {
+    var c = color("rgb(57, 102, 128, " + this.lifeSpan + ")");
+    stroke(c);
+    line(this.position.x, this.position.y, pmouseX, pmouseY);
+};
 
-/*
-* Helps in determining whether the shift()
-* method should be called or not
-*
-* [This method is important, if not used the array will
-* be filled infinitely]
-*/
-Rays.prototype.isDead = function() {
-  if( this.lifeSpan < 0.1 )
-    return true;
-  else
-    return false;
-}
+Rays.prototype.isDead = function () {
+    if (this.lifeSpan < 0.1) return true;
+    else return false;
+};
--- js/frontal.js
+++ js/frontal.js
@@ -1,7 +1,7 @@
-const textBehind = document.getElementById('text-behind');
-const textFront = document.getElementById('text-front');
-const textBehindBlur = document.getElementById('text-behind-blur');
-const canvasRect = document.getElementById('canvas');
+const textBehind = document.getElementById("text-behind");
+const textFront = document.getElementById("text-front");
+const textBehindBlur = document.getElementById("text-behind-blur");
+const canvasRect = document.getElementById("canvas");
 
 const parallaxScaling1 = 0.002;
 const parallaxScaling2 = 0.001;
@@ -14,31 +14,34 @@ let ease = 0.001;
 let theta1 = 0;
 
 function updateScale() {
-  let rect = canvasRect.getBoundingClientRect();
-  
-  let startScrollPosition = window.scrollY + rect.top; 
-  let endScrollPosition = window.scrollY + rect.bottom;
+    let rect = canvasRect.getBoundingClientRect();
 
-  if (targetScroll + window.innerHeight < startScrollPosition || targetScroll > endScrollPosition) {
-     return;
-  }
-  
-  currentScroll += (targetScroll - currentScroll) * ease;
-  
-  let scaleValue1 = 1 + (currentScroll * parallaxScaling1);
-  let scaleValue2 = 1 + (currentScroll * parallaxScaling2);
-    
-  textBehind.style.transform = `scale(${scaleValue1})`;
-  textFront.style.transform = `scale(${scaleValue1})`;
-  textBehindBlur.style.transform = `scale(${scaleValue1})`;
-  canvasRect.style.transform = `scale(${scaleValue2})`;
-  
-  theta1 += currentScroll * parallaxScaling3;
-    
-  setTimeout(updateScale, 1500 / 60); 
+    let startScrollPosition = window.scrollY + rect.top;
+    let endScrollPosition = window.scrollY + rect.bottom;
+
+    if (
+        targetScroll + window.innerHeight < startScrollPosition ||
+        targetScroll > endScrollPosition
+    ) {
+        return;
+    }
+
+    currentScroll += (targetScroll - currentScroll) * ease;
+
+    let scaleValue1 = 1 + currentScroll * parallaxScaling1;
+    let scaleValue2 = 1 + currentScroll * parallaxScaling2;
+
+    textBehind.style.transform = `scale(${scaleValue1})`;
+    textFront.style.transform = `scale(${scaleValue1})`;
+    textBehindBlur.style.transform = `scale(${scaleValue1})`;
+    canvasRect.style.transform = `scale(${scaleValue2})`;
+
+    theta1 += currentScroll * parallaxScaling3;
+
+    setTimeout(updateScale, 1500 / 60);
 }
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
     targetScroll = window.scrollY;
     updateScale();
 });
