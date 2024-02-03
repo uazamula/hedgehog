9cd8714fa3de2411c1f27077bab28cdf4ae73836
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2024-02-03 14:21:20 +0200

    Improve code using blackbox ai

--- js/buttons.js
+++ js/buttons.js
@@ -1,15 +1,13 @@
 $(function () {
     $(".btn-6")
-        .on("mouseenter", function (e) {
-            let parentOffset = $(this).offset(),
-                relX = e.pageX - parentOffset.left,
-                relY = e.pageY - parentOffset.top;
-            $(this).find("span").css({ top: relY, left: relX });
-        })
-        .on("mouseout", function (e) {
-            let parentOffset = $(this).offset(),
-                relX = e.pageX - parentOffset.left,
-                relY = e.pageY - parentOffset.top;
-            $(this).find("span").css({ top: relY, left: relX });
-        });
-});
+      .on("mouseenter", handleHover)
+      .on("mouseout", handleHover);
+  
+    function handleHover(e) {
+      let parentOffset = $(this).offset();
+      let relX = e.pageX - parentOffset.left;
+      let relY = e.pageY - parentOffset.top;
+  
+      $(this).find("span").css({ top: relY, left: relX });
+    }
+  });
