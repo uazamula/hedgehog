73b741a123668dc4e8a1f38a6a42d9665d9b7a1d
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2024-02-03 14:18:26 +0200

    Fomat files

--- js/help.js
+++ js/help.js
@@ -3,18 +3,22 @@ const openModal = () => {
     const modal = document.getElementById("helpModal");
     modalText.innerHTML = settings[levelIndex]["help"];
     modal.style.display = "flex";
-}
+};
 
 const closeModal = () => {
     const modal = document.getElementById("helpModal");
     modal.style.display = "none";
-}
+};
 
 function attachModalEventListeners() {
     const openButton = document.getElementById("helpBtn");
     const closeButton = document.getElementById("closeModal");
-    openButton.addEventListener("click", () => {openModal()});
-    closeButton.addEventListener("click", () => {closeModal()});
+    openButton.addEventListener("click", () => {
+        openModal();
+    });
+    closeButton.addEventListener("click", () => {
+        closeModal();
+    });
 }
 
 attachModalEventListeners();
