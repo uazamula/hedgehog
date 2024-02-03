4db63d521f33aa30baca0023c0cdfd801647aa9c
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2024-02-03 14:20:05 +0200

    Fix code

--- js/buttons.js
+++ js/buttons.js
@@ -1,13 +1,13 @@
 $(function () {
     $(".btn-6")
         .on("mouseenter", function (e) {
-            var parentOffset = $(this).offset(),
+            let parentOffset = $(this).offset(),
                 relX = e.pageX - parentOffset.left,
                 relY = e.pageY - parentOffset.top;
             $(this).find("span").css({ top: relY, left: relX });
         })
         .on("mouseout", function (e) {
-            var parentOffset = $(this).offset(),
+            let parentOffset = $(this).offset(),
                 relX = e.pageX - parentOffset.left,
                 relY = e.pageY - parentOffset.top;
             $(this).find("span").css({ top: relY, left: relX });
