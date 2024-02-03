92157bd55aebc1d41b24b34712bba091d1e4c11c
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2024-02-03 14:21:41 +0200

    Format code using prettier

--- js/buttons.js
+++ js/buttons.js
@@ -1,13 +1,11 @@
 $(function () {
-    $(".btn-6")
-      .on("mouseenter", handleHover)
-      .on("mouseout", handleHover);
-  
+    $(".btn-6").on("mouseenter", handleHover).on("mouseout", handleHover);
+
     function handleHover(e) {
-      let parentOffset = $(this).offset();
-      let relX = e.pageX - parentOffset.left;
-      let relY = e.pageY - parentOffset.top;
-  
-      $(this).find("span").css({ top: relY, left: relX });
+        let parentOffset = $(this).offset();
+        let relX = e.pageX - parentOffset.left;
+        let relY = e.pageY - parentOffset.top;
+
+        $(this).find("span").css({ top: relY, left: relX });
     }
-  });
+});
